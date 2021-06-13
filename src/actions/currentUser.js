771a67ee583410aa1c4fc836ba293dc4bbd227ea
github username <github email address>
2021-06-13 12:06:36 -0400

    start fetch request for login post to sessions controller

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,9 +1,35 @@
+//synchronous action creators
 const url = "http://localhost:3000"
+const loginPath = "api/v1/login"
 
 export const setCurrentUser = (user) => ({type: 'SET_CURRENT_USER', payload: user})
 
+//asynchronous action creators (AKA we need to send fetch to backend)
+export const login = (credentials) => {
+    return dispatch => {
+
+        const configObj = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accepts": "application/json"
+            },
+            body: JSON.stringify({email: "defaultuser@gmail.com", password: "defaultuser"})
+        }
+
+        return fetch(url + "/" + loginPath, configObj)
+    }
+}
+
+//thunk allows us to return a function
+//a function receives dispatch as an argument
+//then we can return a fetch and use dispatch as needed
 
 
 
 
-//action is an object
+
+
+
+//actions are objects
+//action creators receive an argument, which takes the place of our second argument of payload
