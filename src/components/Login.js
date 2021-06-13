d023706186ae58dea20a680b5ac20ed5781849f3
github username <github email address>
2021-06-13 12:28:51 -0400

    add login component

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const Login = () => {
+    return (
+        <form>
+            <input>
+            </input>
+        </form>
+    )
+
+};
+
+export default Login;
