34cbc2316522780bf722a7b4a48fbb12836f3ec8
github username <github email address>
2021-06-13 13:06:31 -0400

    adccess mapStateToPProps for loginForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import './App.css';
+import Login from './components/Login.js'
 
 class App extends Component{
 
   render(){
     return (
       <div>
-       "inside app"
+       <Login/>
       </div>
     )
   };
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from "react-redux";
 
 const Login = () => {
     return (
         <form>
-            <input type="text" name="email" value="" alt="email input" onChange={this.handleClick}>
+            <input type="text" name="email" placeholder="email" value="" alt="email input">
             </input>
-            <input type="text" name="password" value="" alt="password input" onChange={this.handleClick}>
+            <input type="text" name="password" placeholder="password" value="" alt="password input">
             </input>
             <input type="Submit" value="Login"></input>
         </form>
     )
-
 };
 
-export default Login;
+const mapStateToProps = state => {
+    return {
+        email: state.loginForm.username,
+        password: state.loginForm.password
+    };
+};
+
+export default connect(mapStateToProps)(Login);
+
+
+//our login state lives in the store, so we'll need to connect to redux
