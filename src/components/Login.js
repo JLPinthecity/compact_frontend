0caf3e406c995e10f16128b3df583f3571a7fbd0
github username <github email address>
2021-06-13 12:31:43 -0400

    complete login form component

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react'
 const Login = () => {
     return (
         <form>
-            <input>
+            <input type="text" name="email" value="" alt="email input" onChange={this.handleClick}>
             </input>
+            <input type="text" name="password" value="" alt="password input" onChange={this.handleClick}>
+            </input>
+            <input type="Submit" value="Login"></input>
         </form>
     )
 
