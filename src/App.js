a45f0c55f02d176557d3d6e4f1e1d7b1aa6ab3fc
github username <github email address>
2021-06-19 17:41:37 -0400

    add route and pass prouter props to itemcard component to show item showpage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { getCurrentUser } from './actions/currentUser.js';
 import Login from "./components/Login.js";
 import Items from "./components/Items.js";
 import About from "./components/About.js";
+import ItemCard from "./components/ItemCard.js";
 import Home from "./components/Home.js";
 import Signup from "./components/Signup.js";
 import ItemForm from "./components/ItemForm.js";
@@ -16,6 +17,7 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+
 class App extends Component{
   
   componentDidMount(){
@@ -24,6 +26,7 @@ class App extends Component{
 
   render(){
     const history = createBrowserHistory();
+    const { items } = this.props
     // console.log(history)
     return (
       <div>
@@ -37,13 +40,25 @@ class App extends Component{
           <Route history={history} exact path="/signup" component={Signup}/>
           <Route history={history} exact path="/login" component={Login}/>
           <Route exact path="/signin" component={SignupLoginContainer}/>
+          <Route exact path="/items/:id" render={props => {
+              const item = items.items.find(item => item.id === props.match.params.id)
+              return <ItemCard item={item} {...props}/>
+            }
+          }/>
         </Switch>
       </div>
     )
   };
 }
 
-export default withRouter((connect(null, { getCurrentUser })(App)));
+const mapStateToProps = state => {
+  return ({
+    items: state.items
+  })
+}
+
+export default withRouter((connect(mapStateToProps, { getCurrentUser })(App)));
 
 
 // console.log("current user is", this.props.currentUser)
+//render gives you the option of passing all router props or excluding all of them. when we pass props, we are spec not excluding any. 
--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -3,6 +3,8 @@ import React from 'react';
 const ItemCard = ({item}) => {
 
     return (
+
+        item ? 
         <section className="gear"> 
             <div className="product-image">
                 <img alt="hiking gear" src= {item.attributes.image} />
@@ -10,12 +12,15 @@ const ItemCard = ({item}) => {
             
             <div class="product-info">
                 <h3>{item.attributes.name}</h3>
+                <h3>Category: { item.category }</h3>
                 <h4>Price: ${item.attributes.price}</h4>
-                <h4>Purchased: { item.attributes.purchased ? "no" : "yes"  }</h4>
                 <h4>Quantity: {item.attributes.quantity}</h4>
                 <h4>Weight: {item.attributes.weight} ounces </h4>
+                <h4>Purchased: { item.attributes.purchased ? "no" : "yes"  }</h4>
+                <h4>Worn on person or sent home?: { item.attributes.sent_home ? "no" : "yes"  }</h4>
             </div>
         </section>
+        : null
     )
 }
 
