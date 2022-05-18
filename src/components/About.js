a1d75cf4e28bfcfcfe2dd014a8a7079924eeedb5
Joann Pan <joannlpan@gmail.com>
2022-05-18 13:18:53 -0400

    set up layout with flexboxes on about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,82 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
 
 const About = () => {
-    return (
-        <div>
-            "This is the about page."
-        </div>
-    )
-}
+  return (
+    <div className="about-container">
+      <div className="about-main">
+        <div className="row top">
+          <div className="column left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Natoque
+            penatibus et magnis dis parturient. Ipsum faucibus vitae aliquet nec
+            ullamcorper sit amet risus nullam. Mi bibendum neque egestas congue
+            quisque egestas diam. Quis viverra nibh cras pulvinar mattis nunc
+            sed. Curabitur vitae nunc sed velit. Vitae justo eget magna
+            fermentum iaculis. Duis tristique sollicitudin nibh sit amet commodo
+            nulla facilisi. Vitae turpis massa sed elementum tempus egestas sed
+            sed risus. Amet facilisis magna etiam tempor orci eu. Facilisi cras
+            fermentum odio eu feugiat. Pulvinar mattis nunc sed blandit libero
+            volutpat sed cras ornare. Tellus molestie nunc non blandit massa
+            enim. Sit amet mauris commodo quis imperdiet.`
+          </div>
 
-export default About
+          <div className="column right">
+            Bibendum enim facilisis gravida neque convallis a cras semper
+            auctor. Velit aliquet sagittis id consectetur purus ut. Sed velit
+            dignissim sodales ut. Lorem ipsum dolor sit amet consectetur. Nullam
+            vehicula ipsum a arcu cursus. Sed lectus vestibulum mattis
+            ullamcorper velit sed ullamcorper morbi. Vestibulum sed arcu non
+            odio euismod lacinia. Nisi quis eleifend quam adipiscing vitae
+            proin. Sed arcu non odio euismod lacinia. Urna molestie at elementum
+            eu. Quis enim lobortis scelerisque fermentum dui faucibus. Ipsum
+            dolor sit amet consectetur adipiscing elit pellentesque habitant
+            morbi. Suspendisse interdum consectetur libero id faucibus nisl. Sed
+            viverra tellus in hac habitasse platea dictumst vestibulum rhoncus.
+            Eget duis at tellus at urna condimentum mattis pellentesque id.
+            Tellus in hac habitasse platea. Dolor sed viverra ipsum nunc
+            aliquet. Pretium fusce id velit ut. Ullamcorper dignissim cras
+            tincidunt lobortis.
+          </div>
+        </div>
+
+        <div className="row top">
+          <div className="column left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Natoque
+            penatibus et magnis dis parturient. Ipsum faucibus vitae aliquet nec
+            ullamcorper sit amet risus nullam. Mi bibendum neque egestas congue
+            quisque egestas diam. Quis viverra nibh cras pulvinar mattis nunc
+            sed. Curabitur vitae nunc sed velit. Vitae justo eget magna
+            fermentum iaculis. Duis tristique sollicitudin nibh sit amet commodo
+            nulla facilisi. Vitae turpis massa sed elementum tempus egestas sed
+            sed risus. Amet facilisis magna etiam tempor orci eu. Facilisi cras
+            fermentum odio eu feugiat. Pulvinar mattis nunc sed blandit libero
+            volutpat sed cras ornare. Tellus molestie nunc non blandit massa
+            enim. Sit amet mauris commodo quis imperdiet.`
+          </div>
+
+          <div className="column right">
+            Bibendum enim facilisis gravida neque convallis a cras semper
+            auctor. Velit aliquet sagittis id consectetur purus ut. Sed velit
+            dignissim sodales ut. Lorem ipsum dolor sit amet consectetur. Nullam
+            vehicula ipsum a arcu cursus. Sed lectus vestibulum mattis
+            ullamcorper velit sed ullamcorper morbi. Vestibulum sed arcu non
+            odio euismod lacinia. Nisi quis eleifend quam adipiscing vitae
+            proin. Sed arcu non odio euismod lacinia. Urna molestie at elementum
+            eu. Quis enim lobortis scelerisque fermentum dui faucibus. Ipsum
+            dolor sit amet consectetur adipiscing elit pellentesque habitant
+            morbi. Suspendisse interdum consectetur libero id faucibus nisl. Sed
+            viverra tellus in hac habitasse platea dictumst vestibulum rhoncus.
+            Eget duis at tellus at urna condimentum mattis pellentesque id.
+            Tellus in hac habitasse platea. Dolor sed viverra ipsum nunc
+            aliquet. Pretium fusce id velit ut. Ullamcorper dignissim cras
+            tincidunt lobortis.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
