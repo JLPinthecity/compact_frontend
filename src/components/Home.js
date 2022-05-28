8222ba39bdb42237234ce7b2e8f59e8ab511e352
Joann Pan <joannlpan@gmail.com>
2022-05-28 16:24:40 -0400

    add layout to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home-container">
       <div className="home-main">
-        <div></div>
+        <div className="horizontal-section">
+          <div className="image-main">test</div>
+
+          <div className="intro">test</div>
+        </div>
       </div>
     </div>
   );
