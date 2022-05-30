a80c3c82fccf3a3f14c30b6389b86ade8688aeae
Joann Pan <joannlpan@gmail.com>
2022-05-30 14:27:16 -0400

    work on layering images in grid container in home component and import cropped images

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import "./Home.css";
 import woman from "../images/home/sunset1.jpg";
 import man from "../images/home/sunset2.jpg";
 import comb from "../images/home/organic_1.svg";
-import yellow_drop from "../images/yellow.svg";
+import green_curve from "../images/green_curve.svg";
 import orange_squiggly from "../images/organic_2_squiggly.svg";
 import corner_piece from "../images/organic_5_squiggly.svg";
 import pink_drop from "../images/pink drop.svg";
@@ -12,31 +12,48 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="home-main">
-        <div className="horizontal-section">
-          <div className="image-main">
-            <div className="main-grid-item">
-              <img
-                className="female-hiker-image"
-                alt="a female hiker sitting on top of mountain and staring in the distance"
-                src={woman}
-              />
-            </div>
-            <div className="main-grid-item">
-              <img className="purple-graphic" alt="purple graphic" src={man} />
-            </div>
-            <div className="main-grid-item">
-              <img className="purple-graphic" alt="purple graphic" src={comb} />
-            </div>
-            <div className="main-grid-item">
-              <img
-                className="purple-graphic"
-                alt="purple graphic"
-                src={pink_drop}
-              />
-            </div>
+        <div className="image-main">
+          <div className="main-grid-item">
+            <img className="curve" alt="purple graphic" src={corner_piece} />
+          </div>
+          <div className="main-grid-item">
+            <img
+              className="hiker"
+              alt="a female hiker sitting on top of mountain and staring in the distance"
+              src={woman}
+            />
+          </div>
+          <div className="main-grid-item">
+            <img className="hiker" alt="purple graphic" src={man} />
           </div>
 
-          <div className="intro">test</div>
+          <div className="main-grid-item">
+            <img
+              className="purple-graphic"
+              alt="purple graphic"
+              src={green_curve}
+            />
+          </div>
+          <div className="main-grid-item">
+            <img className="purple-graphic" alt="purple graphic" src={comb} />
+          </div>
+          <div className="main-grid-item">
+            <img
+              className="purple-graphic"
+              alt="purple graphic"
+              src={orange_squiggly}
+            />
+          </div>
+          <div className="main-grid-item">
+            <img
+              className="purple-graphic"
+              alt="purple graphic"
+              src={corner_piece}
+            />
+          </div>
+          <div className="main-grid-item">
+            <p>Prep for the adventure of your life.</p>
+          </div>
         </div>
       </div>
     </div>
