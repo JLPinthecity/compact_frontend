38f068a8ad812ccf02be3078fa73198af7ba0e24
Joann Pan <joannlpan@gmail.com>
2022-06-22 15:26:50 -0400

    add menu items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 class Navbar extends React.Component {
   render() {
-    return <nav className="NavbarItems"></nav>;
+    return (
+      <nav className="nav-container">
+        <div className="menu-items">
+          <Link to="/" className="footer-menu-item">
+            Home
+          </Link>
+
+          <Link to="/about" className="footer-menu-item">
+            About
+          </Link>
+
+          <Link to="/items" className="footer-menu-item">
+            Gear List
+          </Link>
+
+          <Link to="/login" className="footer-menu-item">
+            Login
+          </Link>
+        </div>
+      </nav>
+    );
   }
 }
 
