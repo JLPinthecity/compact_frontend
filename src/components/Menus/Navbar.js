53e1860e8c2b3e88d0454bcf49eda9715e3e3ebe
Joann Pan <joannlpan@gmail.com>
2022-06-24 16:35:25 -0400

    import close icon from mui and start on sidebar modal

--- src/components/Menus/Navbar.js
+++ src/components/Menus/Navbar.js
@@ -1,33 +1,51 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { closeSidebar } from "../../actions/showSidebar";
+import CloseIcon from "@mui/icons-material/Close";
 import Logout from "../Logout";
 import "../Menus/Navbar.css";
 
 const Navbar = (props) => {
-  debugger;
+  // props.showSidebar is false until burger icon is clicked then it's true
+
+  const closeSidebar = () => {
+    props.closeSidebar();
+  };
+
   return (
     <nav className="nav-container">
-      <div className="menu">
-        <Link to="/" className="item">
-          Home
-        </Link>
+      <div id={props.menuButtonClicked ? "hidden" : ""}>
+        <div className="menu-row">
+          <span className={props.menuButtonClicked ? "menu-title" : "hide"}>
+            Menu
+          </span>
 
-        <Link to="/about" className="item">
-          About
-        </Link>
-
-        <Link to="/items" className="item">
-          Create Gear List
-        </Link>
-
-        {props.currentUser ? (
-          <Logout className="item" />
-        ) : (
-          <Link to="/login" className="item">
-            Login
+          <span className={props.menuButtonClicked ? "menu-close" : "hide"}>
+            <CloseIcon onClick={closeSidebar} />
+          </span>
+        </div>
+        <div className={props.showSidebar ? "menu-column" : "menu"}>
+          <Link to="/" className="item">
+            Home
           </Link>
-        )}
+
+          <Link to="/about" className="item">
+            About
+          </Link>
+
+          <Link to="/items" className="item">
+            Create Gear List
+          </Link>
+
+          {props.currentUser ? (
+            <Logout className="item" />
+          ) : (
+            <Link to="/login" className="item">
+              Login
+            </Link>
+          )}
+        </div>
       </div>
     </nav>
   );
@@ -40,4 +58,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Navbar);
+export default connect(mapStateToProps, { closeSidebar })(Navbar);
