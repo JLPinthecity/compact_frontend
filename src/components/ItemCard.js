c1c3590b403bdd18b1415819a813b3a3accf1897
Joann Pan <joannlpan@gmail.com>
2022-06-27 10:05:27 -0400

    add responsive layout to item card

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -6,28 +6,30 @@ const ItemCard = ({ item }) => {
   // debugger
   return item ? (
     <section className="item-container">
-      <div className="product-image">
-        <img alt="hiking gear" src={item.attributes.image} />
-      </div>
+      <div className="item-main">
+        <div className="image-left">
+          <img alt="hiking gear" src={item.attributes.image} />
+        </div>
 
-      <div className="product-info">
-        <h3>
-          <Link to={`/items/${item.id}`}>{item.attributes.name}</Link>
-        </h3>
-        <h3>Category: {item.attributes.category.name}</h3>
-        <h3>Description: {item.attributes.notes}</h3>
-        <h4>Price: ${item.attributes.price}</h4>
-        <h4>Quantity: {item.attributes.quantity}</h4>
-        <h4>Weight: {item.attributes.weight} ounces </h4>
-        <h4>Purchased: {item.attributes.purchased ? "no" : "yes"}</h4>
-        <h4>
-          Worn on person or sent home?:{" "}
-          {item.attributes.sent_home ? "no" : "yes"}
-        </h4>
+        <div className="product-info-right">
+          <h3>
+            <Link to={`/items/${item.id}`}>{item.attributes.name}</Link>
+          </h3>
+          <h3>Category: {item.attributes.category.name}</h3>
+          <h3>Description: {item.attributes.notes}</h3>
+          <h4>Price: ${item.attributes.price}</h4>
+          <h4>Quantity: {item.attributes.quantity}</h4>
+          <h4>Weight: {item.attributes.weight} ounces </h4>
+          <h4>Purchased: {item.attributes.purchased ? "no" : "yes"}</h4>
+          <h4>
+            Worn on person or sent home?:{" "}
+            {item.attributes.sent_home ? "no" : "yes"}
+          </h4>
 
-        <Link to={`/items/${item.id}/edit`}>
-          <button className="btn">Edit item</button>
-        </Link>
+          <Link to={`/items/${item.id}/edit`}>
+            <button className="btn">Edit item</button>
+          </Link>
+        </div>
       </div>
     </section>
   ) : (
